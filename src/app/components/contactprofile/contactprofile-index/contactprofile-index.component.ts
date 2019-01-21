dfd637526b0eb7454bf9d52f3220ecf5a08ff4fe
Edycarlos018 <edycarlos@gmail.com>
2019-01-21 10:00:21 -0500

    contact view is working

--- src/app/components/contactprofile/contactprofile-index/contactprofile-index.component.ts
+++ src/app/components/contactprofile/contactprofile-index/contactprofile-index.component.ts
@@ -3,52 +3,23 @@ import { ContactProfileService } from 'src/app/services/contactprofile.service';
 import { ContactProfile } from 'src/app/models/ContactProfile';
 import { MatTableDataSource } from '@angular/material';
 
-
-
 @Component({
   selector: 'app-contactprofile-index',
   templateUrl: './contactprofile-index.component.html',
   styleUrls: ['./contactprofile-index.component.css']
 })
-export class ContactProfileIndexComponent implements OnInit {
-  columnNames = ['FirstName', 'LastName' ];
-  dataSource: MatTableDataSource<ContactProfile>
-  userId: string
-  token: any
-  decodedToken: any
 
-  constructor(private _contactProfileServices: ContactProfileService) { }
+export class ContactProfileIndexComponent implements OnInit {
+    dataSource: ContactProfile;  
+  
+  constructor(private _contactProfileService: ContactProfileService) { }
 
   ngOnInit() {
-    this.token=localStorage.getItem('id_token')
-    this.userId=this.decodedToken.nameid
-    this._contactProfileServices.getContactProfile(this.userId).subscribe((contactProfile: ContactProfile[]) => {
-      this.dataSource = new MatTableDataSource<ContactProfile>(contactProfile);
+    // console.log("ngOnInit")
+    this._contactProfileService.getContactProfile('1002').subscribe((contact: ContactProfile) => {
+      this.dataSource = contact;
       console.log(this.dataSource)
+      console.log("Contact", contact);
     });
   }
-
 }
-
-// import { Component, OnInit } from '@angular/core';
-// import { ContactProfileService } from 'src/app/services/contactprofile.service';
-// import { ContactProfile } from 'src/app/models/ContactProfile';
-// import { MatTableDataSource } from '@angular/material';
-
-// @Component({
-//   selector: 'app-contactprofile-index',
-//   templateUrl: './contactprofile-index.component.html',
-//   styleUrls: ['./contactprofile-index.component.css']
-// })
-// export class ContactProfileIndexComponent implements OnInit {
-//   columnNames = ['FirstName', 'LastName' ]
-//   dataSource: MatTableDataSource<ContactProfile>
-
-//   constructor(private _contactProfileService: ContactProfileService) { }
-
-//   ngOnInit() {
-//     this._contactProfileService.getContact().subscribe((contactProfile: ContactProfile[]) => {
-//       this.dataSource = new MatTableDataSource<ContactProfile>(contactProfile);
-//     });
-//   }
-// }
