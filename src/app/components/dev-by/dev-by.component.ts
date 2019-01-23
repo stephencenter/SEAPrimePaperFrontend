c8c4cddf70257834768bae667e4cd28ac0edd9b9
Aidan Vandiver <skrtAidan@gmail.com>
2019-01-22 23:25:49 -0500

    added dev by and stylings

---
+++ src/app/components/dev-by/dev-by.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-dev-by',
+  templateUrl: './dev-by.component.html',
+  styleUrls: ['./dev-by.component.css']
+})
+export class DevByComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
